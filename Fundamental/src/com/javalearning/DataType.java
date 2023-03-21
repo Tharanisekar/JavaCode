f9ade876909a86e93f5937e76d77f8fc932f098a
USER <USER@DESKTOP-VSO1LP1>
2023-03-21 14:52:32 +0530

    first commit

---
+++ Fundamental/src/com/javalearning/DataType.java
@@ -0,0 +1,27 @@
+package com.javalearning;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		int a = 10;
+		byte b = 20;
+		short c = 30;
+		long d = 50;
+		float e = 200f;
+		double f  = 250;
+		char g = 'a';
+		
+	
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+
+	}
+
+}
